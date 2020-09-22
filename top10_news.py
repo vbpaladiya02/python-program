d88980a4c684e30d8974a0aa6805116666793d60
vivek paladiya <70934508+vivekpaladiya@users.noreply.github.com>
2020-09-22 16:01:14 +0530

    files uploaded

---
+++ top10_news.py
@@ -0,0 +1,28 @@
+import requests
+import json
+i=1
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+url="http://newsapi.org/v2/top-headlines?country=in&apiKey=15243095bd224fe6b23bf976e982048f"
+
+nr=requests.get(url).text
+news=json.loads(nr)
+article=news['articles']
+
+speak("top 10 news of india for today.. listen carefully")
+for art in article:
+    print(i,".")
+    speak(f"{i} news is..")
+    print(art['title'])
+    speak(art['title'])
+    print("\n")
+    i+=1
+    if i==11:
+        break
+    
+speak("thank you for listening good bye....")
+
+ 
